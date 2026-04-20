5e181906e99588ff63df3497eec95b07150a14b4
odon-airoldi <odon.airoldi@gmail.com>
2026-04-20 21:52:38 +0200

    fix query reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -20,8 +20,10 @@ const index = (req, res) => {
 const show = (req, res) => {
 
     const query_sql = 'SELECT * FROM movies WHERE id = ?'
-    const query_sql_reviews = 'SELECT * FROM reviews WHERE book_id = ?'
+    const query_sql_reviews = 'SELECT * FROM reviews WHERE movie_id = ?'
+
     const id = Number(req.params.id)
+
 
     connection.query(query_sql, [id], (err, results) => {
         if (err) {
@@ -38,20 +40,16 @@ const show = (req, res) => {
                 console.error(err)
                 return res.status(500).json({ error: 'query error' })
             }
-            if (results.length === 0) {
-                console.error(err)
-                return res.status(404).json({ error: 'movie not found' })
-            }
 
             // join
             results[0].reviews = resultsReview
 
             res.json(results[0])
 
-        });
+        })
 
 
-    });
+    })
 
 }
 
